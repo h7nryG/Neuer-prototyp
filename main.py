bb854d18aaac6690dd7d628f78818e4f0f7eed39
Jonas <99035978+Johnny-tech-droid@users.noreply.github.com>
2024-10-28 09:10:52 +0100

    initial commit zweiter prototyp pfad

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ import os
 def clear_console():
     if os.name == 'nt':
         os.system('cls')
+        # wenn es nicht windows ist, dann wird es als posix erkannt
     elif os.name == 'posix':
+        # wenn die variable TERM in os.environ ist, dann wird es als clear erkannt
         os.system('clear')
 
-
 def show_default_section():
+    """
+    This function shows the default path of the game
+    :return:
+    """
     section_read = 0
     total_sections = len(content.erster_prototyp_pfad)
     for contents in content.erster_prototyp_pfad:
@@ -23,6 +28,12 @@ def show_default_section():
 
 
 def show_section(section_key):
+    """
+    This function shows the section of the game
+    :param section_key:
+    :return:
+    """
+    clear_console()
     section = individual.chapter[section_key]
     wrapped_section = textwrap.fill(section['content'], width=100)
     print(wrapped_section)
@@ -32,10 +43,18 @@ def show_section(section_key):
 
 
 def get_user_input():
+    """
+    This function gets the user input
+    :return:
+    """
     return input("Wähle deine nächsten Schritte: ")
 
 
 def init_game():
+    """
+    This function initializes the game
+    :return:
+    """
     current_section = 'abs1'
     while True:
         options = show_section(current_section)
@@ -48,6 +67,10 @@ def init_game():
 
 
 def choose_paths():
+    """
+    This function lets the user choose the path either individual or the default path
+    :return:
+    """
     user_choice_path = input("Wähle deinen Pfad: \n1.Individueller Pfad\n2.Standard Pfad\nAuswahl: ")
     if user_choice_path == "1":
         init_game()
